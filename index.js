f2bac0b354d949e71b6a8943b6e6032ce09c8b16
URL <sonupanditbth2003@gmail.com>
2026-03-18 23:35:13 +0530

    github data added

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.get('/twitter', (req, res) => {
+//     res.send('Sonu Pandit')
+// })
+
+// app.get('/login', (req, res) => {
+//     res.send('<h1>plese login at chai aur code</h1>')
+// })
+// app.get('/youtube', (req, res) => {
+//     res.send('<h2>chai aur code</h2>')
+// })
+// app.listen(process.env.PORT, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+
+
+
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
 
+const port = process.env.PORT || 3000 
 
+const githubData = {
+
+    user: [
+{
+"login": "mojombo",
+"id": 1,
+"node_id": "MDQ6VXNlcjE=",
+"avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+"gravatar_id": "",
+"url": "https://api.github.com/users/mojombo",
+"html_url": "https://github.com/mojombo",
+"followers_url": "https://api.github.com/users/mojombo/followers",
+"following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+"gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+"starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+"subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+"organizations_url": "https://api.github.com/users/mojombo/orgs",
+"repos_url": "https://api.github.com/users/mojombo/repos",
+"events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+"received_events_url": "https://api.github.com/users/mojombo/received_events",
+"type": "User",
+"user_view_type": "public",
+"site_admin": false
+}],
+
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -14,11 +72,17 @@ app.get('/twitter', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.send('<h1>plese login at chai aur code</h1>')
+    res.send('<h1>Please login at chai aur code</h1>')
 })
+
 app.get('/youtube', (req, res) => {
     res.send('<h2>chai aur code</h2>')
 })
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
